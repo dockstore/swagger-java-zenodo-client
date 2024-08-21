de267b769d95dfe817c15ebf41d7f73a9be1d511
Charles Overbeck <coverbec@ucsc.edu>
2024-08-21 14:37:14 -0700

    Add the concept DOI to SearchResult
    dockstore/dockstore#5745
    DOCK-2493

--- src/test/java/io/dockstore/ZenodoClientTest.java
+++ src/test/java/io/dockstore/ZenodoClientTest.java
@@ -1,19 +1,39 @@
 package io.dockstore;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import io.swagger.zenodo.client.ApiClient;
+import io.swagger.zenodo.client.model.SearchResult;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
-
-
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
 public class ZenodoClientTest {
 
-    @org.junit.jupiter.api.Test
-    public void testZenodoClient() throws io.swagger.zenodo.client.ApiException {
-        io.swagger.zenodo.client.ApiClient client = new io.swagger.zenodo.client.ApiClient();
+    private static ApiClient client;
+    private static io.swagger.zenodo.client.api.PreviewApi previewApi;
+
+    @BeforeAll
+    static void setup() {
+        client = new ApiClient();
         client.setBasePath("https://sandbox.zenodo.org/api");
-        io.swagger.zenodo.client.api.PreviewApi previewApi = new io.swagger.zenodo.client.api.PreviewApi(client);
+        previewApi = new io.swagger.zenodo.client.api.PreviewApi(client);
+    }
+
+    @Test
+    @Disabled("Test disabled as the communities API is failing on sandbox")
+    public void testZenodoClient() throws io.swagger.zenodo.client.ApiException {
         HashMap<String, Object> o = (HashMap<String, Object>)previewApi.listCommunities();
         assertTrue(o != null && !o.keySet().isEmpty(), "not able to list communities as basic test");
+    }
+
+    @Test
+    void testConceptDoi() {
+        final SearchResult searchResult = previewApi.listRecords(null, "bestmatch", 1, 100);
+        assertNotNull(searchResult.getHits().getHits().get(0).getConceptdoi());
     }
 }
