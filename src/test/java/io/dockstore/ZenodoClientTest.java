6471dce35361b77431419be2af06a565d8fae478
Charles Overbeck <coverbec@ucsc.edu>
2024-08-21 14:55:05 -0700

    Upgrade Surefire version so annotations work

--- src/test/java/io/dockstore/ZenodoClientTest.java
+++ src/test/java/io/dockstore/ZenodoClientTest.java
@@ -4,21 +4,21 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import io.swagger.zenodo.client.ApiClient;
+import io.swagger.zenodo.client.ApiException;
+import io.swagger.zenodo.client.api.PreviewApi;
 import io.swagger.zenodo.client.model.SearchResult;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class ZenodoClientTest {
 
-    private static ApiClient client;
-    private static io.swagger.zenodo.client.api.PreviewApi previewApi;
+    private ApiClient client;
+    private PreviewApi previewApi;
 
-    @BeforeAll
-    static void setup() {
+    @BeforeEach
+    void setup() {
         client = new ApiClient();
         client.setBasePath("https://sandbox.zenodo.org/api");
         previewApi = new io.swagger.zenodo.client.api.PreviewApi(client);
@@ -26,7 +26,7 @@ public class ZenodoClientTest {
 
     @Test
     @Disabled("Test disabled as the communities API is failing on sandbox")
-    public void testZenodoClient() throws io.swagger.zenodo.client.ApiException {
+    public void testZenodoClient() throws ApiException {
         HashMap<String, Object> o = (HashMap<String, Object>)previewApi.listCommunities();
         assertTrue(o != null && !o.keySet().isEmpty(), "not able to list communities as basic test");
     }
